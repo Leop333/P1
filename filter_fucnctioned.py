9683708c9b6eef11c74f2457800d54d3ec96ce5b
Leop333 <42681924+Leop333@users.noreply.github.com>
2023-02-11 12:08:18 +0300

    Update filter_fucnctioned.py
    Filt_result added to basic fucmtions as argument and returned by them.Final output process added.

--- filter_fucnctioned.py
+++ filter_fucnctioned.py
@@ -12,6 +12,7 @@ from nltk.tokenize import word_tokenize, wordpunct_tokenize
 from nltk.stem.snowball import SnowballStemmer
 from thefuzz import fuzz
 stemmer = SnowballStemmer("russian")
+
 ##### 0 emulate inputs
 # original text
 with open("rbk_orig.txt", "r", encoding='utf-8') as orig:
@@ -43,12 +44,12 @@ with open("rbk_inp4.txt", "r", encoding='utf-8') as inp4:
 orig_len=len(orig)
 filt_result={}
 #create filter DF
-
+filt_result={}
 #list of obscene prepositions
 prep_list=["не","по","твою","мать"]
 
 #check 1 length
-def volume_check(input_1,orig_volume):
+def volume_check(input_1,orig_volume,filt_result):
   inp_vol= len(input_1)
   if (inp_vol/orig_volume) > 1.1000:
     filt_result["vol_chk"]=1
@@ -59,29 +60,32 @@ def volume_check(input_1,orig_volume):
   else:
     filt_result["vol_chk"]=0
     filt_result["vol_comm"]=""
+  return filt_result
 #CHECK
-volume_check(inp2,orig_len)
+volume_check(inp2,orig_len,filt_result)
 filt_result
-
+volume_check(inp,orig_len,filt_result)
+filt_result
 #check2 text similarity
-def similarity_check(input_1,original_text):
+def similarity_check(input_1,original_text,filt_result):
   sim_rat=fuzz.ratio(original_text,input_1)
   if sim_rat < 90:
     filt_result["sml_chk"] = "1"
     filt_result["sml_comm"] = "Different for more than 10%. Similarity rate is "+str(sim_rat)
   else:
     filt_result["sml_chk"] = "0"
-    filt_result["sml_comm"] = "Similarity rate is "+str(sim_rat) 
+    filt_result["sml_comm"] = "Similarity rate is "+str(sim_rat)
+  return filt_result
 #CHECK
-similarity_check(inp2,orig)
+similarity_check(inp2,orig,filt_result)
 filt_result["sml_chk"]
 filt_result["sml_comm"]
-similarity_check(inp3,orig)
+similarity_check(inp3,orig,filt_result)
 filt_result["sml_chk"]
 filt_result["sml_comm"]
 
 #check3 html tagging
-def html_check (input_1):
+def html_check (input_1,filt_result):
   html_tags=re.findall ("<[^>]+>",string=input_1)
   htg_len=len(html_tags)
   if htg_len > 0:
@@ -90,16 +94,17 @@ def html_check (input_1):
   else:
     filt_result["html_chk"] = "0"
     filt_result["html_text"] = ""
+  return filt_result
 #CHECK
-html_check(inp)
+html_check(inp,filt_result)
 filt_result["html_chk"] 
 filt_result["html_text"]
-html_check(inp4)
+html_check(inp4,filt_result)
 filt_result["html_chk"] 
 filt_result["html_text"]
 
 #check4 obscene lexis
-def obscene_check (input_1):
+def obscene_check (input_1,filt_result):
   #token-stem
   inp_tk=input_1.translate(str.maketrans("", "", string.punctuation))  
   inp_tk= wordpunct_tokenize(inp_tk)
@@ -131,12 +136,13 @@ def obscene_check (input_1):
     filt_result["obscene_chk"] = "0"
     filt_result["obscene_text"] = ""
     filt_result["obscene_text_orignal"] = ""
+  return filt_result
 #CHECK
-obscene_check (inp)
+obscene_check (inp,filt_result)
 filt_result["obscene_chk"]
 filt_result["obscene_text"]
 filt_result["obscene_text_orignal"]
-obscene_check (inp4)
+obscene_check (inp4,filt_result)
 filt_result["obscene_chk"]
 filt_result["obscene_text"]
 filt_result["obscene_text_orignal"]
@@ -146,21 +152,32 @@ filt_result["obscene_text_orignal"]
 def filter_text (input_1,original_text,orig_volume,person_id):
   filt_result={}
   filt_result["ID"]=person_id
-  volume_check(input_1,orig_volume)
-  similarity_check(input_1,original_text)
-  html_check (input_1)
-  obscene_check (input_1)
+  volume_check(input_1,orig_volume,filt_result)
+  similarity_check(input_1,original_text,filt_result)
+  html_check (input_1,filt_result)
+  obscene_check (input_1,filt_result)
   return filt_result
 
 
 
-#check output
-f_df = pd.DataFrame()
-cur=filter_text(inp,orig,orig_len,"001")
-cur=pd.DataFrame(cur,index=[0])
-pd.concat(f_df, cur, ignore_index=True)
+#Output function short
+#input simulation 
+works_list = [inp, inp2, inp3, inp4]
+person_id = ['001', '002', '003', '004']
+colnames = ['ID', 'vol_chk', 'vol_comm', 'sml_chk', 'sml_comm', 'html_chk', 'html_text', 'obscene_chk', 'obscene_text', 'obscene_text_orignal']
 
-f_df.to_csv("Filter_results.csv", sep=';',encoding="CP1251")
+list_res = []
+# processing loop
+for i in range(len(works_list)):
+    inp_dict = filter_text(works_list[i],orig,orig_len,person_id[i])
+    dict_val = list(inp_dict.values())
+    list_res.append(dict_val)
+
+#output
+df = pd.DataFrame(list_res, columns=colnames)
+df.to_csv("Filter_results.csv", sep=';',encoding="CP1251")
+
+
 
 
 
